6ab63b14f1c4aba7e93ba3698852dac6a2eb2e32
10570708 <83276752+10570708@users.noreply.github.com>
2022-04-13 08:46:45 +0100

    Changes to Swagger - removing enum translation

--- Program.cs
+++ Program.cs
@@ -10,12 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddControllers().AddJsonOptions(options =>
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => { c.SchemaFilter<EnumSchemaFilter>(); });
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 string connString = builder.Configuration.GetConnectionString("NewsReportAPIServiceContext");
 builder.Services.AddDbContext<NewsReportAPIServiceContext>(opt => opt.UseSqlServer(connString));
@@ -38,17 +36,3 @@ app.MapControllers();
 
 app.Run();
 
-public class EnumSchemaFilter : ISchemaFilter
-{
-    public void Apply(OpenApiSchema model, SchemaFilterContext context)
-    {
-        if (context.Type.IsEnum)
-        {
-            model.Enum.Clear();
-            Enum.GetNames(context.Type)
-                .ToList()
-                .ForEach(name => model.Enum.Add(new OpenApiString($"{Convert.ToInt64(Enum.Parse(context.Type, name))} = {name}")));
-        }
-    }
-
-}
